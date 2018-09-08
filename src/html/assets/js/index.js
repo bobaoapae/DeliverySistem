cc4380189a848542964ba5e362bd5581f9b8e242
joao borges <jvborges@outlook.com>
2018-09-08 12:15:53 -0300

    Mudanças na responsividade da inde

--- src/html/assets/js/index.js
+++ src/html/assets/js/index.js
@@ -3,15 +3,19 @@ $(document).ready(function () {
     $("#sidebarCollapse").on("click", function () {
         $("#sidebar").toggleClass("active");
         $(this).toggleClass("active");
+        $("#content").css("maxWidth", ($(".wrapper").width() - ($("#sidebar").hasClass("active") ? 250 : 0)) + "px");
+        window.setTimeout(() => {
+            $("#topVendidos").highcharts().reflow();
+        }, 300);
     });
-    $("#content").css("maxWidth", ($(".wrapper").width() - 250) + "px");
+    $("#content").css("maxWidth", ($(".wrapper").width() - ($("#sidebar").css("margin-left") == "-250px" ? 0 : 250)) + "px");
     window.setTimeout(() => {
         $("#topVendidos").highcharts().reflow();
     }, 300);
 });
 
 $(window).resize(function () {
-    $("#content").css("maxWidth", ($(".wrapper").width() - 250) + "px");
+    $("#content").css("maxWidth", ($(".wrapper").width() - ($("#sidebar").css("margin-left") == "-250px" ? 0 : 250)) + "px");
     window.setTimeout(() => {
         $("#topVendidos").highcharts().reflow();
     }, 300);
